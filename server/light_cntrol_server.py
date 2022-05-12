92b3fb0a362534338f05631e9f7beecbeaccb926
Yoshitake Kitamura <kitamura@epea.co.jp>
2022-05-12 19:32:23 +0900

    とりあえずサーバ

---
+++ server/light_cntrol_server.py
@@ -0,0 +1,21 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import parse_qs, urlparse
+
+address = ('localhost', 8080)
+
+class MyHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print('path = {}'.format(self.path))
+
+        parsed_path = urlparse(self.path)
+        print('parsed: path = {}, query = {}'.format(parsed_path.path, parse_qs(parsed_path.query)))
+
+        print('headers\r\n-----\r\n{}-----'.format(self.headers))
+       
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/plain; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(b'Hello from do_GET')
+
+with HTTPServer(address, MyHTTPRequestHandler) as server:
+    server.serve_forever()
