2832307349d7bf488f2f92c4108d8b5ece9c3b22
Yoshitake Kitamura <kitamura@epea.co.jp>
2022-05-27 18:52:39 +0900

    時刻判定処理
    とりあえず22時でお客さんいても消灯

--- server/light_cntrol_server.py
+++ server/light_cntrol_server.py
@@ -1,14 +1,22 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import parse_qs, urlparse
+from datetime import time, datetime
+
 
 address = ('192.168.1.13', 8080)
+def is_open():
+    night = time(22,00,0)
+    current_time = datetime.now().time()
+    return current_time < night 
 
 class MyHTTPRequestHandler(BaseHTTPRequestHandler):
+
+
     def do_GET(self):
         parsed_path = urlparse(self.path)
      
         sensor_value = int(parse_qs(parsed_path.query)['data'][0])
-        light= 'ON' if sensor_value <3000 else 'OFF'
+        light= 'ON' if ((sensor_value <3000) and is_open()) else 'OFF'
 
         self.send_response(200)
         self.send_header('Content-Type', 'text/plain; charset=utf-8')
